28f616e3ee3036e178d8d1516aa05b131196ef1f
Primal-Drew <oginaandrew05@gmail.com>
2024-01-20 18:40:33 +0300

    Using Conditions

---
+++ Conditions.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    //Adding to two numbers 
+    int a=6;
+    int b=5;
+    int c;
+    c=a+b;
+    //Introducing the if condtion
+    if(c==11){
+        //if(condtion_here){The function if true}
+        printf("Impressed by the addition? %d ",c);
+    }else {
+        //Executes when the if condtion comes out as true
+            printf("Not impressed");        
+    }
+} 
