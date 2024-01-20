9c1ccb48530beaac2f4b5316fb15983b74a9a8bd
Primal-Drew <oginaandrew05@gmail.com>
2024-01-20 17:28:31 +0300

    basic Arithmetics

---
+++ Calculations.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // In c you must initialize the data type of a variable
+    int a=3;
+    int b=6;
+    int c;
+    c=a+b;
+
+    printf("%d",c);
+
+    int multi;
+    multi = a*b;
+
+    printf("\n%d",multi);
+
+
+
+
+}
