b1456f81fef93fe9ea17e4db3f65cf53b5d025bd
Primal-Drew <oginaandrew05@gmail.com>
2024-01-24 14:21:15 +0300

    Fibonacci sequence... Crushes after 46

---
+++ Fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int fibo(){
+    int n;
+    printf("Enter:");
+    scanf("%d",&n);
+    int fibosqn[n];
+        fibosqn[0]=0;
+        fibosqn[1]=1;
+    
+        if(n==0){
+            return fibosqn[0];
+
+        }else if(n==1){
+            return fibosqn[1];
+
+        }else{
+            for(int i=2;i<=n;i++){
+                fibosqn[i]=fibosqn[i-1]+fibosqn[i-2];
+                }
+        return fibosqn[n];
+        
+        
+    }
+    }
+int main(){
+    int exit;
+    int f = fibo();
+    printf("%d",f);
+
+    scanf("%d",&exit);
+}
