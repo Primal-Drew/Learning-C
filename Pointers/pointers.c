e4cf3afb9170bbb410c2bf4152965ff3da9a37f3
primal-drew <oginaandrew05@gmail.com>
2024-01-31 10:37:06 +0300

    Pointers in c

--- Pointers/pointers.c
+++ Pointers/pointers.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
- int main(){
-    int c,*pc;
+int main()
+{
+   int c, *pc;
 
-    c=5;
-    pc = &c;
-    printf("\p",pc);
-    printf("\n\n%d",pc);
+   c = 5;
+
+   pc = &c;
+
+   printf("Value of c:%d",c);
+
+   printf("Adress of c:%p\n",&c);
+
+   printf("Address pc points to:%p\n", pc);
+
+   printf("Actual address of pc:%p\n",&pc);
+
+   printf("Value at pc;%d\n\n\n", *pc);
 
 
- }
+   printf("CHANGING THE VALUES USING A POINTERS.\n\n");
+
+   *pc = 10;
+
+   printf("New value of c:%d\n",c);
+
+   int* p1;
+
+   p1= (pc+1);
+
+   printf("Value of p+1;%p\n",&p1);
+
+   printf("Using p1 to derefence c\n");
+
+   *(p1-1) = 7;
+
+   printf("Value of c:%d",c);
+
+
+
+   return 0;
+}
